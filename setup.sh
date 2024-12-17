58022750d5dbe92b3845cc7ef3ce57611c157a2f
SkyZ18 <114467762+SkyZ18@users.noreply.github.com>
2024-12-17 12:06:28 +0100

    Added Script for startup

--- setup.sh
+++ setup.sh
@@ -7,25 +7,34 @@ BPurple='\033[1;34m'
 #-------------------
 
 #Entry point
-echo "[${BPurple}INFO${Color_Off}] --- ${Green}Starting Setup${Color_Off} ---"
+echo "[${BPurple}INFO${Color_Off}] --- ${Green}Starting setup${Color_Off} ---"
 
+CURRENT_DIR=$(pwd)
 SHELL_DIR=$(dirname "$0")
 OS=$(uname -s)
-echo "${OS}"
 
-echo "[${BPurple}INFO${Color_Off}] --- ${Green}Initiating Database${Color_Off} ---"
+echo "[${BPurple}INFO${Color_Off}] --- ${Green}Initiating database${Color_Off} ---"
 
 result=$( docker ps -a -q -f name="monetaDatabase" )
 
 if [ "$result" != "" ]; then
   echo "[${BPurple}INFO${Color_Off}] --- ${Green}Container already exists${Color_Off} ---"
 else
-  echo "[${BPurple}INFO${Color_Off}] --- ${Green}Creating Container${Color_Off} ---"
+  echo "[${BPurple}INFO${Color_Off}] --- ${Green}Creating container${Color_Off} ---"
   cd "$SHELL_DIR" || exit
 
   if [ "$OS" == "Darwin" ]; then
-    sudo docker compose up
+    sudo docker compose up -d
   elif [ "$OS" == "Msys" ]; then
-    docker compose up
+    docker compose up -d
   fi
 fi
+
+echo "[${BPurple}INFO${Color_Off}] --- ${Green}Finished database setup${Color_Off} ---"
+echo "[${BPurple}INFO${Color_Off}] --- ${Green}Setting up environment${Color_Off} ---"
+
+cd moneta-ui/ || exit
+npm ci
+
+cd "${CURRENT_DIR}" || exit
+echo "[${BPurple}INFO${Color_Off}] --- ${Green}Script finished${Color_Off} ---"
